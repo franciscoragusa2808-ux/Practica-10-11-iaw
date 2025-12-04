2d4bddcad9b50b3cc86cbd3e6ad03cb40e707471
Ubuntu <ubuntu@ip-172-31-17-138.ec2.internal>
2025-12-04 11:03:37 +0000

    se sube

--- scripts/install_lamp_frontend.sh
+++ scripts/install_lamp_frontend.sh
@@ -18,8 +18,8 @@ a2enmod rewrite
 #Reiniciamos el servicio de apache
 systemctl restart apache2
 
-#Copiamos el archivo de configuración de apache
-cp ../conf/000-default.conf /etc/apache2/sites-available
+Copiamos el archivo de configuración de apache
+#cp ../conf/000-default.conf /etc/apache2/sites-available
 
 #Instalamos PHP y el módulo de apache para PHP y el módulo de MySQL para PHP
 sudo apt install php libapache2-mod-php php-mysql -y
@@ -28,7 +28,7 @@ sudo apt install php libapache2-mod-php php-mysql -y
 systemctl restart apache2
 
 #copiamos el archivo de index.php  a/var/www/html
-cp ../php/index.php /var/www/html/
+#cp ../php/index.php /var/www/html/
 
 #cambiamos el propietario de /var/www/html a www-data
 chown -R www-data:www-data /var/www/html/
